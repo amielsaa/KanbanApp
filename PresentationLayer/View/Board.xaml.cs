82922bd5485d2facd60b7a0e973ab170bedf4cf4
Amiel <amielsaa@post.bgu.ac.il>
2021-06-06 01:46:50 +0300

    adding boardview and taskview

---
+++ PresentationLayer/View/Board.xaml.cs
@@ -0,0 +1,44 @@
+﻿using IntroSE.Kanban.PresentationLayer.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IntroSE.Kanban.PresentationLayer.View
+{
+    /// <summary>
+    /// Interaction logic for Board.xaml
+    /// </summary>
+    public partial class Board : Window
+    {
+
+        private BoardVM boardVM;
+
+        public Board()
+        {
+            InitializeComponent();
+            this.DataContext = boardVM;
+        }
+
+        private void Button_Add_Task(object sender, RoutedEventArgs e)
+        {
+            Task task = new Task();
+            task.Show();
+
+        }
+
+        private void Button_Add_Column(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
